82b1e64f1adb969c01b49f4a3cae79791c140ca7
psto <piotrstojan@gmail.com>
2019-05-07 23:04:01 +0200

    complete part 14 of awesome node app

--- awesome-node/public/dist/App.bundle.js
+++ awesome-node/public/dist/App.bundle.js
@@ -63,11 +63,38 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return; // skip this fn from running if there is not input on the page
+  var dropdown = new google.maps.places.Autocomplete(input);
+
+  dropdown.addListener('place_changed', function () {
+    var place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+  // if someone hits enter on the address field, don't submit the form
+  input.on('keydown', function (e) {
+    if (e.keyCode === 13) e.preventDefault();
+  });
+}
+
+exports.default = autocomplete;
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -97,21 +124,29 @@ exports.$ = $;
 exports.$$ = $$;
 
 /***/ }),
-/* 1 */
+/* 2 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: Cannot find module 'node-sass'\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:611:15)\n    at Function.Module._load (internal/modules/cjs/loader.js:537:25)\n    at Module.require (internal/modules/cjs/loader.js:665:17)\n    at require (internal/modules/cjs/helpers.js:20:18)\n    at Object.<anonymous> (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/sass-loader/lib/loader.js:3:14)\n    at Module._compile (internal/modules/cjs/loader.js:736:30)\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:747:10)\n    at Module.load (internal/modules/cjs/loader.js:628:32)\n    at tryModuleLoad (internal/modules/cjs/loader.js:568:12)\n    at Function.Module._load (internal/modules/cjs/loader.js:560:3)\n    at Module.require (internal/modules/cjs/loader.js:665:17)\n    at require (internal/modules/cjs/helpers.js:20:18)\n    at loadLoader (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/loadLoader.js:13:17)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:165:10)\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:173:18\n    at loadLoader (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/loadLoader.js:36:3)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:165:10)\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:173:18\n    at loadLoader (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/loadLoader.js:36:3)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\n    at runLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:362:2)\n    at NormalModule.doBuild (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModule.js:129:2)\n    at NormalModule.build (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModule.js:180:15)\n    at Compilation.buildModule (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/Compilation.js:142:10)\n    at moduleFactory.create (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/Compilation.js:424:9)\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModuleFactory.js:242:4\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModuleFactory.js:93:13\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/tapable/lib/Tapable.js:268:11\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModule.js:141:35\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:170:18\n    at loadLoader (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/loadLoader.js:27:11)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:165:10)\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:173:18\n    at loadLoader (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/loadLoader.js:36:3)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:165:10)\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:173:18\n    at loadLoader (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/loadLoader.js:36:3)\n    at iteratePitchingLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:169:2)\n    at runLoaders (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/loader-runner/lib/LoaderRunner.js:362:2)\n    at NormalModule.doBuild (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModule.js:129:2)\n    at NormalModule.build (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModule.js:180:15)\n    at Compilation.buildModule (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/Compilation.js:142:10)\n    at moduleFactory.create (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/Compilation.js:424:9)\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModuleFactory.js:242:4\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModuleFactory.js:93:13\n    at /home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/tapable/lib/Tapable.js:268:11\n    at NormalModuleFactory.params.normalModuleFactory.plugin (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/CompatibilityPlugin.js:52:5)\n    at NormalModuleFactory.applyPluginsAsyncWaterfall (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/tapable/lib/Tapable.js:272:13)\n    at onDoneResolving (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModuleFactory.js:68:11)\n    at onDoneResolving (/home/piotrek/workspace/learning/node/practice_node/awesome-node/node_modules/webpack/lib/NormalModuleFactory.js:189:6)\n    at processTicksAndRejections (internal/process/next_tick.js:74:9)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
-/* 2 */
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(1);
+__webpack_require__(2);
 
-var _bling = __webpack_require__(0);
+var _bling = __webpack_require__(1);
+
+var _autocomplete = __webpack_require__(0);
+
+var _autocomplete2 = _interopRequireDefault(_autocomplete);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _autocomplete2.default)((0, _bling.$)('#address'), (0, _bling.$)('#lat'), (0, _bling.$)('#lng'));
 
 /***/ })
 /******/ ]);
