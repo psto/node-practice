0cefb991416fce4b4049c74bf2820d55a59335e1
psto <piotrstojan@gmail.com>
2019-05-29 22:04:26 +0200

    complete part 29 of awesome node app

--- awesome-node/controllers/storeController.js
+++ awesome-node/controllers/storeController.js
@@ -163,3 +163,8 @@ exports.getHearts = async (req, res) => {
   });
   res.render('stores', { title: 'Hearted Stores', stores });
 };
+
+exports.getTopStores = async (req, res) => {
+  const stores = await Store.getTopStores();
+  res.render('topStores', { stores, title:'⭐ Top Stores!'});
+}
