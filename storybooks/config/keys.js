1c1b58c35dc4ce7234b7a546a50c93ef6fe07ebc
psto <psto@users.noreply.github.com>
2020-02-21 17:49:55 +0100

    complete part 1 of storybooks

---
+++ storybooks/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mlab or other database link',
+  googleClientID: 'PASTE YOUR GOOGLE_CLIEND_ID here',
+  googleClientSecret: 'PASTE YOUR GOOGLE_CLIENT_SECRET here'
+}
